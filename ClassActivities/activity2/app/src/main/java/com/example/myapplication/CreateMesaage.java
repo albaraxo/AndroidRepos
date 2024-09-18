839583d8b99c06a231b4a5dd5d5fe0b7e50337be
albaraxo <albaraabumansoor@gmail.com>
2024-09-18 14:54:32 +0300

    add class activity two and rename class testAndroid to activity1.

--- ClassActivities/activity2/app/src/main/java/com/example/myapplication/CreateMesaage.java
+++ ClassActivities/activity2/app/src/main/java/com/example/myapplication/CreateMesaage.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,11 +27,18 @@ public class CreateMesaage extends AppCompatActivity {
     }
     public void onSendMessage(View view) {
         // get the from textField element, then store the value in variable.
-        EditText messageView = findViewById(R.id.message);
-        String messageText = messageView.getText().toString();
+        EditText messageView = findViewById(R.id.id);
+        String textID = messageView.getText().toString();
+        EditText messageView2 = findViewById(R.id.studentName);
+        String textName = messageView2.getText().toString();
+        Spinner mySpinner = (Spinner) findViewById(R.id.spn);
+        String spnText = mySpinner.getSelectedItem().toString();
+
         // create new Intent, send message to the that class, then start it.
         Intent intent = new Intent(this, RecieveMessage.class);
-        intent.putExtra(RecieveMessage.EXTRA_MESSAGE, messageText);
+        intent.putExtra(RecieveMessage.GetID, textID);
+        intent.putExtra(RecieveMessage.GetName, textName);
+        intent.putExtra(RecieveMessage.GetSpen,spnText);
         startActivity(intent);
     }
 
--- ClassActivities/activity2/app/src/main/java/com/example/myapplication/RecieveMessage.java
+++ ClassActivities/activity2/app/src/main/java/com/example/myapplication/RecieveMessage.java
@@ -9,15 +9,28 @@ import android.widget.TextView;
 
 public class RecieveMessage extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "message";
+    public static final String GetID = "key1";
+    public static final String GetName = "key2";
+    public static final String GetSpen = "key3";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ///
         Intent intent = getIntent();
-        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
-        TextView messageView = findViewById(R.id.message);
+
+        String messageText = "The ID: " + intent.getStringExtra(GetID);
+        TextView messageView = findViewById(R.id.message1);
         messageView.setText(messageText);
+
+        String messageStudent = "the Name: "+ intent.getStringExtra(GetName);
+        TextView messageView2 = findViewById(R.id.message2);
+        messageView2.setText(messageStudent);
+
+        String messgageSpn = "the Major: " + intent.getStringExtra(GetSpen);
+        TextView messageView3 = findViewById(R.id.message3);
+        messageView3.setText(messgageSpn);
     }
 }
